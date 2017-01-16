5d7dd0ae8984ea2847146225c33d3b5416339534
jahlela <jahlela5@gmail.com>
2017-01-15 19:35:38 -0800

    Finished coin change

---
+++ making_change.py
@@ -0,0 +1,16 @@
+
+def my_change(total, coins):
+    ways = [1] + [0]*(total)
+
+    # Iterate over options (which coin to use now)
+    for coin in coins:
+        # Add the ways this coin could be added to each value from coin to the end
+        for i in range(coin, total + 1):
+            ways[i] += ways[i - coin]
+
+    return ways[total]
+
+
+total = 15
+coins = [1,2,3]
+print my_change(total, coins)
